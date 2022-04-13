6069c3e264303d68da1ac1f0983c099a0a52bccd
Gavdzinskyi <veacheslavgavdzynsky@gmail.com>
2022-04-13 21:28:21 +0300

    removed secret key

--- django_movie/django_movie/settings.py
+++ django_movie/django_movie/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-yu!x@jd7jh+)fe0kc3yr_@6#w*uj8!z_)1xa2z!$igqg5a++'
+SECRET_KEY = '--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

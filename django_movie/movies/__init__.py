2b2929ec9d7a766205f66dcca8cbdd81900a4048
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-18 23:01:40 +0300

    Displaying images in the admin. Lesson 12

---
+++ django_movie/movies/__init__.py
@@ -0,0 +1,1 @@
+default_app_confing = 'movies.apps.MoviesConfing'
--- django_movie/movies/admin.py
+++ django_movie/movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from django.utils.safestring import mark_safe
 from .models import Category, Genre, Movie, MovieShots, Actor, Rating, RatingStar, Reviews
 
 @admin.register(Category)
@@ -14,22 +14,32 @@ class ReviewInline(admin.TabularInline):
     extra = 1
     readonly_fields = ("name", "email")
 
+class MovieShotsInline(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="200" height="110"')
+    get_image.short_description = "Изображение"
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     """Фильмы"""
     list_display = ("title", "category", "url", "draft")
     list_filter = ("category", "year")
     search_fields = ("title", "category__name")
-    inlines = [ReviewInline]
+    inlines = [MovieShotsInline, ReviewInline]
     save_on_top = True
     save_as = True
     list_editable = ("draft", )
+    readonly_fields = ("get_image",)
     fieldsets = (
         (None, {
             "fields": (("title", "tagline"), )
         }),
         (None, {
-            "fields": ("description", "poster")
+            "fields": ("description", ("poster", "get_image"))
         }),
         (None, {
             "fields": (("year", "world_premier", "country"), )
@@ -46,6 +56,10 @@ class MovieAdmin(admin.ModelAdmin):
         }),
 
     )
+    
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="200"')
+    get_image.short_description = "Постер"
 
 @admin.register(Reviews)
 class ReviewAdmin(admin.ModelAdmin):
@@ -62,7 +76,12 @@ class GenreAdmin(admin.ModelAdmin):
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
     """Актёры"""
-    list_display = ("name", "age")
+    list_display = ("name", "age", "get_image")
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+    get_image.short_description = "Изображение"
 
 @admin.register(Rating)
 class RatingAdmin(admin.ModelAdmin):
@@ -72,9 +91,15 @@ class RatingAdmin(admin.ModelAdmin):
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
     """Кадры из фильма"""
-    list_display = ("title", "movie")
+    list_display = ("title", "movie", "get_image", )
+    readonly_fields = ("get_image",)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="50" height="60"')
+    get_image.short_description = "Изображение"
 
 
 admin.site.register(RatingStar)
 
-
+admin.site.site_title = "Django Movies"
+admin.site.site_header = "Django Movies"

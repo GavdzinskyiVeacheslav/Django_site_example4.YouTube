2513e494fa099a0ec4532ab19c951e665ff8c815
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-20 15:29:45 +0300

    added ckeditor and plugin youtube. Lesson 13

--- django_movie/django_movie/urls.py
+++ django_movie/django_movie/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include("movies.urls")),
 ]
 

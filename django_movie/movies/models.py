a02638ad21fabb01aa3350802a450c1a4979367a
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-21 23:43:30 +0300

    added actor page, attribute related_name. Lesson 16

--- django_movie/movies/models.py
+++ django_movie/movies/models.py
@@ -27,6 +27,9 @@ class Actor(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('actor_detail', kwargs={"slug": self.name})
+
     class Meta:
         verbose_name = "Актёры и режиссёры"
         verbose_name_plural = "Актёры и режиссёры"
--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 
 
-from .models import Movie, Category
+from .models import Actor, Movie, Category
 from .forms import ReviewForm
 
 class MoviesView(ListView):
@@ -31,4 +31,10 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+class ActorView(DetailView):
+    """Getting information about an actor"""
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = "name"
    

63e1e56dbb4693c63581f16f2dab1424e9c37333
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-23 15:56:16 +0300

    added movie filter, Q. Lesson 17

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -1,26 +1,36 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, redirect
 from django.views import View
+from django.db.models import Q
 
 
-from .models import Actor, Movie, Category
+from .models import Actor, Movie, Category, Genre
 from .forms import ReviewForm
 
-class MoviesView(ListView):
-    """Список фильмов"""
+class GenreYear:
+    """Film genres and release years"""
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
+    """List of films"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
     
 		 
-class MovieDetailView(DetailView):
-    """Полное описание фильма"""
+class MovieDetailView(GenreYear, DetailView):
+    """Full movie description"""
     model = Movie
     slug_field = "url"
 
 
 class AddReview(View):
-    """Отзывы"""
+    """Reviews"""
     def post(self, request, pk):
         form = ReviewForm(request.POST)
         movie = Movie.objects.get(id=pk)
@@ -32,9 +42,20 @@ class AddReview(View):
             form.save()
         return redirect(movie.get_absolute_url())
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Getting information about an actor"""
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """"Movie filter"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) | 
+            Q(genres__in=self.request.GET.getlist("genre"))
+            )
+        return queryset
+
    

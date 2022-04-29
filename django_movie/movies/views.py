1e9df747482e4c14ca2a8c6f878884a3601d0114
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-04-29 15:23:35 +0300

    Lesson_20_Pagination_Django

--- django_movie/movies/views.py
+++ django_movie/movies/views.py
@@ -22,6 +22,7 @@ class MoviesView(GenreYear, ListView):
     """List of films"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
+    paginate_by = 3
 
     
 		 
@@ -58,12 +59,19 @@ class ActorView(GenreYear, DetailView):
 
 class FilterMoviesView(GenreYear, ListView):
     """"Movie filter"""
+    paginate_by = 2
     def get_queryset(self):
         queryset = Movie.objects.filter(
             Q(year__in=self.request.GET.getlist('year')) | 
             Q(genres__in=self.request.GET.getlist("genre"))
-            )
+            ).distinct()
         return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['year'] = ''.join([f'year={x}&' for x in self.request.GET.getlist('year')])
+        context['genre'] = ''.join([f'genre={x}&' for x in self.request.GET.getlist('genre')])
+        return context
 
 
 class JsonFilterMoviesView(ListView):
